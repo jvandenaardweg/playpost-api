10ce6a79b03f76e6b463369bb4ae91c981b250cb
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-02-21 09:46:40 +0100

    Several eslint tweaks

--- extractor.js
+++ extractor.js
@@ -1,7 +1,7 @@
-const extractors = require('./extractors/readability');
 const fetch = require('node-fetch');
+const extractors = require('./extractors/readability');
 
 fetch('https://medium.com/@Trott/using-worker-threads-in-node-js-80494136dbb6')
-.then((response) => response.text())
-.then((html) => extractors.htmlToText(html))
-.then((text) => console.log(text))
+  .then(response => response.text())
+  .then(html => extractors.htmlToText(html))
+  .then(text => console.log(text));
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -12,40 +13,51 @@ const storage = require('./storage/google-cloud');
 
 global.appRoot = path.resolve(__dirname);
 
-app.get('/audiofile', asyncMiddleware(async (req, res, next) => {
-    const { url } = req.query
+app.get(
+  '/audiofile',
+  asyncMiddleware(async (req, res, next) => {
+    const { url } = req.query;
 
-    if (!url || url === '') throw new Error('Please give a URL param.')
+    if (!url || url === '') throw new Error('Please give a URL param.');
 
-    const normalizedUrl = url.toLowerCase()
+    const normalizedUrl = url.toLowerCase();
 
-    if (!normalizedUrl.includes('medium.com')) throw new Error('We only allow Medium URLs for now.')
+    if (!normalizedUrl.includes('medium.com')) throw new Error('We only allow Medium URLs for now.');
 
     // Get the Medium Post ID from the URL
     // If the URL is incorrect, we error
-    const articleId = await dataSource.getArticleIdFromUrl(normalizedUrl)
+    const articleId = await dataSource.getArticleIdFromUrl(normalizedUrl);
 
     // So in the future we can determine what voice a user wants to use
     // For example: "free" user maybe should use Amazon, because it's cheaper
     // For example: "premium" user maybe should use Google, because it's more expensive
     const synthesizerOptions = {
-        synthesizer: 'Google', // or Amazon
-        languageCode: 'en-US', // or en-GB, en-AU
-        name: 'en-US-Wavenet-D', // or en-GB-Wavenet-A
-        source: 'medium-com' // or cnn-com
-    }
+      synthesizer: 'Google', // or Amazon
+      languageCode: 'en-US', // or en-GB, en-AU
+      name: 'en-US-Wavenet-D', // or en-GB-Wavenet-A
+      source: 'medium-com', // or cnn-com
+    };
 
     // Create an upload path based on the synthesizer options
     // We end up with something like this: google/en-us/en-us-wavenet-d/medium-com
-    const uploadPath = Object.values(synthesizerOptions).map((value) => value.toLowerCase()).join('/');
+    const uploadPath = Object.values(synthesizerOptions)
+      .map(value => value.toLowerCase())
+      .join('/');
 
     // Find an existing file in our cloud storage
-    const existingFiles = await storage.listFilesByPrefix(uploadPath + '/');
-    const foundFile = (existingFiles && existingFiles.length) ? existingFiles.find((file) => file.name.includes(articleId)) : null
+    const existingFiles = await storage.listFilesByPrefix(`${uploadPath}/`);
+    const foundFile = existingFiles && existingFiles.length
+      ? existingFiles.find(file => file.name.includes(articleId))
+      : null;
 
     // If we already have a file in storage, we just return that (for now)
     // TODO: we should not use the cloud storage API for this? use a database? so we an also return information about the article?
-    if (foundFile) return res.json({ publicFileUrl: storage.getPublicFileUrl(foundFile.metadata), article: {} });
+    if (foundFile) {
+      return res.json({
+        publicFileUrl: storage.getPublicFileUrl(foundFile.metadata),
+        article: {},
+      });
+    }
 
     // If we don't have an audiofile, we go into here
 
@@ -56,7 +68,11 @@ app.get('/audiofile', asyncMiddleware(async (req, res, next) => {
     const ssmlParts = await utils.ssml.getSSMLParts(ssml);
 
     // Send the SSML parts to Google's Text to Speech API and download the audio files
-    const localAudiofilePaths = await synthesize.ssmlPartsToSpeech(articleId, ssmlParts, synthesizerOptions);
+    const localAudiofilePaths = await synthesize.ssmlPartsToSpeech(
+      articleId,
+      ssmlParts,
+      synthesizerOptions,
+    );
 
     // Uncomment for local dev testing purposes
     // const localAudiofilePaths = [
@@ -66,21 +82,31 @@ app.get('/audiofile', asyncMiddleware(async (req, res, next) => {
     // ]
 
     // Concatinate the different files into one .mp3 file
-    const concatinatedLocalAudiofilePath = await utils.audio.concatAudioFiles(articleId, localAudiofilePaths, synthesizerOptions);
+    const concatinatedLocalAudiofilePath = await utils.audio.concatAudioFiles(
+      articleId,
+      localAudiofilePaths,
+      synthesizerOptions,
+    );
 
     // const audioFileDurationInSeconds = await utils.audio.getAudioFileDurationInSeconds(concatinatedLocalAudiofilePath);
 
     // Upload the one mp3 file to Google Cloud Storage
-    const publicFileUrl = await storage.uploadFile(concatinatedLocalAudiofilePath, uploadPath);
+    const publicFileUrl = await storage.uploadFile(
+      concatinatedLocalAudiofilePath,
+      uploadPath,
+    );
 
     // TODO: Store all this data in a database
 
     // Cleanup the local audiofiles, we don't need that anymore
-    const removedFile = await utils.local.removeFile(global.appRoot + `/temp/${synthesizerOptions.source}/${articleId}`)
+    const removedFile = await utils.local.removeFile(
+      `${global.appRoot}/temp/${synthesizerOptions.source}/${articleId}`,
+    );
 
     console.log('Done!');
 
     res.json({ publicFileUrl, article });
-}))
+  }),
+);
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
