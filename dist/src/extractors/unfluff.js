468e109e52b19f3b854a2a3e69fd24e3cbb61029
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-02-25 07:56:17 +0100

    .gitignore is now working

--- dist/src/extractors/unfluff.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const unfluff = require('unfluff');
-const htmlToText = (html) => {
-    return unfluff(html);
-};
-module.exports = { htmlToText };
-//# sourceMappingURL=unfluff.js.map
