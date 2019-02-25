f74fa66939522116e0879adddae260ed0961b7f9
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-02-25 15:52:43 +0100

    testing

--- src/index.ts
+++ src/index.ts
@@ -26,9 +26,9 @@ declare global {
   }
 }
 
-// const rootDir = path.resolve(__dirname || process.cwd(), '..');
+const rootDir = path.resolve(__dirname || process.cwd(), '..');
 
-// console.log('App init', 'rootDir', rootDir);
+console.log('App init', 'rootDir', rootDir);
 console.log('App init', 'HEROKU_SLUG_COMMIT', process.env.HEROKU_SLUG_COMMIT);
 
 const PORT = process.env.PORT || 3000;
@@ -43,11 +43,11 @@ if (process.env.NODE_ENV === 'production') {
     dsn: 'https://479dcce7884b457cb001deadf7408c8c@sentry.io/1399178',
     environment: 'production',
     release: (process.env.HEROKU_SLUG_COMMIT) ? process.env.HEROKU_SLUG_COMMIT : '',
-    // integrations: [
-    //   new Sentry.Integrations.RewriteFrames({
-    //     root: rootDir
-    //   })
-    // ]
+    integrations: [
+      new Sentry.Integrations.RewriteFrames({
+        root: '/app'
+      })
+    ]
   });
 
   // The request handler must be the first middleware on the app
