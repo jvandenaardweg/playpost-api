0aa04df5957ed7eb2624dfe5eec61acd43125e1e
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-03-02 17:43:26 +0100

    Make sure the user exists upon using the api endpoints

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,5 +1,7 @@
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { PassportStatic } from 'passport';
+import { User } from '../database/entities/user';
+import { getRepository } from 'typeorm';
 
 const { JWT_SECRET } = process.env;
 
@@ -15,27 +17,16 @@ type StrategyPayload = {
 
 module.exports = (passport: PassportStatic) => {
   return passport.use(
-    new Strategy(opts, (payload: StrategyPayload, done) => {
+    new Strategy(opts, async (payload: StrategyPayload, done) => {
 
-      const { id, email } = payload;
+      const { id } = payload;
 
-      // Send make the id and email available using "req.user.id" and "req.user.email"
-      if (id && email) return done(null, { id, email });
+      const user = await getRepository(User).findOne(id);
 
-      return done(null, false);
-      // Get the user from the database
-      // payload.id is extracted from the JWT token
-      // return prisma.user({ id: payload.id })
-      //   .then((user) => {
-      //     if (!user) return done(null, false);
+      if (!user) return done(null, false);
 
-      //     // Never return the password
-      //     const userWithoutPassword = user;
-      //     delete userWithoutPassword.password;
+      return done(null, { id: user.id, email: user.email });
 
-      //     return done(null, userWithoutPassword);
-      //   })
-      //   .catch(err => console.error(err));
     })
   );
 };
