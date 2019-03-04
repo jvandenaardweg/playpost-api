271125450c22b95e296777a8d80fe1c22fd59bac
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-03-04 15:50:09 +0100

    Use name for typeorm connection, so we can use multiple later on

--- src/database/connection-options.ts
+++ src/database/connection-options.ts
@@ -6,22 +6,25 @@ import { Playlist } from './entities/playlist';
 import { PlaylistItem } from './entities/playlist-item';
 import { Audiofile } from './entities/audiofile';
 
-export const connectionOptions: ConnectionOptions = {
-  type: 'postgres',
-  url: process.env.DATABASE_URL,
-  extra: {
-    ssl: (process.env.NODE_ENV === 'production') ? true : false // For Heroku
-  },
-  logging: (process.env.NODE_ENV === 'production') ? ['error'] : true, // Complete logging in dev, only errors in production
-  synchronize: (process.env.NODE_ENV === 'production') ? false : true, // Sync changes directly when in dev
-  entities: [
-    User,
-    Playlist,
-    PlaylistItem,
-    Article,
-    Audiofile
-  ],
-  migrationsRun: true, // Run migrations on start. So when we deploy to production, migrations run automatically.
-  dropSchema: false,
-  maxQueryExecutionTime: 1000
+export const connectionOptions = (name = 'default'): ConnectionOptions => {
+  return {
+    name,
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    extra: {
+      ssl: (process.env.NODE_ENV === 'production') ? true : false // For Heroku
+    },
+    logging: (process.env.NODE_ENV === 'production') ? ['error'] : true, // Complete logging in dev, only errors in production
+    synchronize: (process.env.NODE_ENV === 'production') ? false : true, // Sync changes directly when in dev
+    entities: [
+      User,
+      Playlist,
+      PlaylistItem,
+      Article,
+      Audiofile
+    ],
+    migrationsRun: true, // Run migrations on start. So when we deploy to production, migrations run automatically.
+    dropSchema: false,
+    maxQueryExecutionTime: 1000
+  };
 };
