0dd124a4180e05649549fb71a220b00a056124f1
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-03-04 15:49:16 +0100

    Added more audio concatination tests

--- src/utils/audio.ts
+++ src/utils/audio.ts
@@ -6,17 +6,13 @@ import fluentFfmpeg from 'fluent-ffmpeg';
 /* eslint-disable no-console */
 
 export const getAudioFileDurationInSeconds = async (audioFilePath: string): Promise<number> => {
-  if (process.env.NODE_ENV !== 'test') {
-    console.log('Get audiofile duration in seconds...');
-  }
+  console.log('Get audiofile duration in seconds...');
 
   return new Promise((resolve, reject) => {
     mp3Duration(audioFilePath, (err: Error, durationInSeconds: number) => {
       if (err) return reject(err);
 
-      if (process.env.NODE_ENV !== 'test') {
-        console.log(`Got audiofile duration: ${durationInSeconds} seconds.`);
-      }
+      console.log(`Got audiofile duration: ${durationInSeconds} seconds.`);
 
       return resolve(durationInSeconds);
     });
@@ -34,33 +30,19 @@ export const concatAudioFiles = async (audioFiles: string[], storageUploadPath: 
     const tempFilePath = `${storageUploadPath}.mp3`;
     const outputPath = `${tempPath}/${tempFilePath}`;
 
-    // If we have just one file, we don't need to concat it
-    // We just remove the index number and return the file
-    if (audioFiles.length === 1) {
-      const audioFile = audioFiles[0];
-      const audioFileNameWithoutIndex = audioFile.split('-0.mp3')[0];
-      const newAudiofileName = `${audioFileNameWithoutIndex}.mp3`;
-
-      return fsExtra.copy(audioFile, outputPath)
-        .then(() => resolve(outputPath));
-    }
-
     // If we have multiple audiofiles, we concat them
     // First, sort correctly
     audioFiles.sort((a: any, b: any) => b - a); // Sort: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 etc...
 
-    if (process.env.NODE_ENV !== 'test') {
-      audioFiles.forEach(audioFile => console.log(`Sorted audiofiles, order: ${audioFile}`));
-
-      console.log(`Combining ${audioFiles.length} audio files to one audio file...`);
-    }
+    audioFiles.forEach(audioFile => console.log(`Sorted audiofiles, order: ${audioFile}`));
+    console.log(`Combining ${audioFiles.length} audio files to one audio file...`);
 
     return fluentFfmpeg()
     .audioCodec('libmp3lame')
     .input(`concat:${audioFiles.join('|')}`)
     .outputOptions('-acodec copy')
     .save(outputPath)
-    .on('error', (err: any) => reject(new Error(err)))
+    .on('error', (err: any) => reject(err))
     .on('end', () => resolve(outputPath));
 
       // return audioconcat(audioFiles)
--- src/utils/tests/audio.test.ts
+++ src/utils/tests/audio.test.ts
@@ -4,6 +4,7 @@ import fsExtra from 'fs-extra';
 
 const exampleFile1 = `${appRootPath}/src/mocks/audio/example-file-1.mp3`;
 const exampleFile2 = `${appRootPath}/src/mocks/audio/example-file-2.mp3`;
+const exampleWrongFile = `${appRootPath}/src/mocks/audio/wrong-file.js`;
 const exampleFileConcatinated = `${appRootPath}/src/mocks/audio/example-concatinated.mp3`;
 const expectedOutput = `${appRootPath}/temp/example-concatinated.mp3`;
 const expectedDurationInSecondsFile1 = 27.742;
@@ -20,13 +21,28 @@ describe('audio', () => {
 
     it('Should concatenate two audiofiles into one.', async () => {
       const filePath = await concatAudioFiles([exampleFile1, exampleFile2], 'example-concatinated');
-
       expect(filePath).toBe(expectedOutput);
     });
 
-    it('Should give the correct audio duration of the concatinated audiofile.', async () => {
-      const durationInSeconds = await getAudioFileDurationInSeconds(expectedOutput);
-      expect(durationInSeconds).toBe(expectedDurationOutput);
+    it('Should also concatinate with 1 audiofile.', async () => {
+      const filePath = await concatAudioFiles([exampleFile1], 'example-concatinated');
+      expect(filePath).toBe(expectedOutput);
+    });
+
+    it('Should throw an error when no files to concatinate are given.', async () => {
+      try {
+        await concatAudioFiles([], 'example-concatinated');
+      } catch (e) {
+        expect(e.message).toBeDefined();
+      }
+    });
+
+    it('Should throw an error when concatinating fails.', async () => {
+      try {
+        await concatAudioFiles([exampleWrongFile, exampleWrongFile], 'example-concatinated');
+      } catch (e) {
+        expect(e.message).toBeDefined();
+      }
     });
 
   });
@@ -44,6 +60,14 @@ describe('audio', () => {
     it('Should give the correct audio duration of the concatinated audiofile.', async () => {
       const durationInSeconds = await getAudioFileDurationInSeconds(exampleFileConcatinated);
       expect(durationInSeconds).toBe(expectedDurationOutput);
+    });
+
+    it('Should throw an error when no file is given.', async () => {
+      try {
+        await getAudioFileDurationInSeconds('');
+      } catch (e) {
+        expect(e.message).toBeDefined();
+      }
     });
   });
 });
