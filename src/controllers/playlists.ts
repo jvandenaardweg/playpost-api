e6dcf34c1e22f0d3bba5f671350fae39b0d1ddca
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-03-10 16:52:34 +0100

    Correctly handle error where article is already in a user's playlist

--- src/controllers/playlists.ts
+++ src/controllers/playlists.ts
@@ -13,7 +13,7 @@ const MESSAGE_PLAYLISTS_DEFAULT_EXISTS = 'There is already a Default playlist fo
 const MESSAGE_PLAYLISTS_NAME_EXISTS = 'There is already a playlist with this name. We cannot create another one. Choose a different name.';
 const MESSAGE_PLAYLISTS_NAME_REQUIRED = 'A name for a playlist is required.';
 const MESSAGE_PLAYLISTS_ARTICLE_NOT_FOUND = 'Article does not exist';
-const MESSAGE_PLAYLISTS_ARTICLE_EXISTS_IN_PLAYLIST = 'Article is already in this playlist.';
+const MESSAGE_PLAYLISTS_ARTICLE_EXISTS_IN_PLAYLIST = 'You already have this article in your playlist!';
 
 export const findAllPlaylists = async (req: Request, res: Response) => {
   const userEmail = req.user.email;
@@ -78,18 +78,33 @@ export const createPlaylistItemByArticleUrl = async (req: Request, res: Response
   const playlistRepository = getRepository(Playlist);
   const articleRepository = getRepository(Article);
 
-  let createdArticle;
+  let createdArticle: Article;
 
+  // Get the playlist to check if it exists
   const playlist = await playlistRepository.findOne(playlistId, { relations: ['user'] });
 
   if (!playlist) return res.status(400).json({ message: MESSAGE_PLAYLISTS_NOT_FOUND });
 
+  // Check if the user is the owner of that playlist
   if (playlist.user.id !== userId) return res.status(403).json({ message: MESSAGE_PLAYLISTS_NO_ACCESS_PLAYLIST });
 
   const article = await articleRepository.findOne({ url: articleUrl });
 
+  // If there's an article, check if that one already exists in the user's playlist
+  if (article) {
+    const playlistItem = await playlistItemRepository.findOne({
+      playlistId,
+      userId,
+      articleId: article.id
+    });
+
+    if (playlistItem) return res.status(400).json({ message: MESSAGE_PLAYLISTS_ARTICLE_EXISTS_IN_PLAYLIST });
+  }
+
   // If we do not have an article yet, fetch the basic details and add it to the database
   if (!article) {
+    // Fetch the article details from the article URL
+    // Important: this is doing a request to the URL
     const articleDetails = await fetchArticleDetails(articleUrl);
 
     if (articleDetails.language !== 'eng') {
@@ -120,16 +135,16 @@ export const createPlaylistItemByArticleUrl = async (req: Request, res: Response
   const articleId = (article) ? article.id : createdArticle.id;
 
   // Create the playlist item
-  const playlistItem = await playlistItemRepository.findOne({
-    article: {
-      id: articleId
-    },
-    playlist: {
-      id: playlistId
-    }
-  });
+  // const playlistItem = await playlistItemRepository.findOne({
+  //   article: {
+  //     id: articleId
+  //   },
+  //   playlist: {
+  //     id: playlistId
+  //   }
+  // });
 
-  if (playlistItem) return res.status(400).json({ message: MESSAGE_PLAYLISTS_ARTICLE_EXISTS_IN_PLAYLIST });
+  // if (playlistItem) return res.status(400).json({ message: MESSAGE_PLAYLISTS_ARTICLE_EXISTS_IN_PLAYLIST });
 
   // TODO: Set correct "order"
   const playlistItemToCreate = await playlistItemRepository.create({
