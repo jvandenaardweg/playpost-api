912b5b1075b35b7c6275233b12437095c478f455
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-03-27 11:08:31 +0100

    Connect worker to database

--- src/worker.ts
+++ src/worker.ts
@@ -1,3 +1,6 @@
+import { createConnection } from 'typeorm';
+
+import { connectionOptions } from './database/connection-options';
 import { redisClientSub } from './cache';
 import { addEmailToMailchimpList, removeEmailToMailchimpList } from './mailers/mailchimp';
 import * as articlesController from './controllers/articles';
@@ -7,46 +10,50 @@ import { ArticleStatus } from './database/entities/article';
 // This happens right after the insert of a new article.
 // Since the crawling of the full article details takes longer...
 // ...we do it right after insertion in the database of the minimal article details
-redisClientSub.on('message', async (channel, message) => {
-  if (channel === 'FETCH_FULL_ARTICLE') {
-    const articleId = message;
-    console.log('Worker process started: ', channel, articleId);
+createConnection(connectionOptions('default')).then(async (connection: any) => {
+  console.log('Worker init:', 'Connected with database', connection.options.url);
 
-    try {
-      await articlesController.updateArticleStatus(articleId, ArticleStatus.CRAWLING);
-      await articlesController.updateArticleToFull(articleId);
-    } catch (err) {
-      console.log('FETCH_FULL_ARTICLE failed.', err);
-      await articlesController.updateArticleStatus(articleId, ArticleStatus.FAILED);
-    } finally {
-      console.log('Worker process ended: ', channel, articleId);
-    }
-  }
+  redisClientSub.on('message', async (channel, message) => {
+    if (channel === 'FETCH_FULL_ARTICLE') {
+      const articleId = message;
+      console.log('Worker process started: ', channel, articleId);
 
-  if (channel === 'ADD_TO_MAILCHIMP_LIST') {
-    const userEmail = message;
-    console.log('Worker process started: ', channel, userEmail);
-
-    try {
-      await addEmailToMailchimpList(userEmail);
-    } catch (err) {
-      console.log('ADD_TO_MAILCHIMP_LIST failed.', err);
-    } finally {
-      console.log('Worker process ended: ', channel, userEmail);
+      try {
+        await articlesController.updateArticleStatus(articleId, ArticleStatus.CRAWLING);
+        await articlesController.updateArticleToFull(articleId);
+      } catch (err) {
+        console.log('FETCH_FULL_ARTICLE failed.', err);
+        await articlesController.updateArticleStatus(articleId, ArticleStatus.FAILED);
+      } finally {
+        console.log('Worker process ended: ', channel, articleId);
+      }
     }
 
-  }
+    if (channel === 'ADD_TO_MAILCHIMP_LIST') {
+      const userEmail = message;
+      console.log('Worker process started: ', channel, userEmail);
 
-  if (channel === 'REMOVE_FROM_MAILCHIMP_LIST') {
-    const userEmail = message;
-    console.log('Worker process started: ', channel, userEmail);
+      try {
+        await addEmailToMailchimpList(userEmail);
+      } catch (err) {
+        console.log('ADD_TO_MAILCHIMP_LIST failed.', err);
+      } finally {
+        console.log('Worker process ended: ', channel, userEmail);
+      }
 
-    try {
-      await removeEmailToMailchimpList(userEmail);
-    } catch (err) {
-      console.log('REMOVE_FROM_MAILCHIMP_LIST failed.', err);
-    } finally {
-      console.log('Worker process ended: ', channel, userEmail);
     }
-  }
+
+    if (channel === 'REMOVE_FROM_MAILCHIMP_LIST') {
+      const userEmail = message;
+      console.log('Worker process started: ', channel, userEmail);
+
+      try {
+        await removeEmailToMailchimpList(userEmail);
+      } catch (err) {
+        console.log('REMOVE_FROM_MAILCHIMP_LIST failed.', err);
+      } finally {
+        console.log('Worker process ended: ', channel, userEmail);
+      }
+    }
+  });
 });
