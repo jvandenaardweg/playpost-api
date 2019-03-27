c3a07ad92ce3e14f571120737bc1b5125de73a7d
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-03-27 14:35:17 +0100

    Added normalize url method

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,3 +1,5 @@
+import normalizeUrl from 'normalize-url';
+
 /**
  * Trims a string at a certain length and makes sure we don't trim it in the middle of a word.
  */
@@ -7,3 +9,15 @@ export const trimTextAtWords = (text: string, maxLength: number = 200): string =
   const trimmedText = text.substr(0, maxLength);
   return `${trimmedText.substr(0, Math.min(trimmedText.length, trimmedText.lastIndexOf(' ')))}...`;
 };
+
+export const getNormalizedUrl = (url: string) => {
+  return normalizeUrl(url, {
+    removeQueryParameters: [/^utm_\w+/i],
+    sortQueryParameters: true,
+    removeTrailingSlash: true,
+    stripWWW: false,
+    stripAuthentication: true,
+    normalizeProtocol: true,
+    defaultProtocol: 'https'
+  });
+}
