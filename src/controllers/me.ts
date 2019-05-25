4d1af46a0fa3ba9931d358e74c47dadcccaf7099
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-05-25 21:42:33 +0200

    Add subscription purchases to me endpoint

--- src/controllers/me.ts
+++ src/controllers/me.ts
@@ -14,7 +14,7 @@ export const findCurrentUser = async (req: Request, res: Response) => {
   const userId = req.user.id;
   const userRepository = getRepository(User);
 
-  const user = await userRepository.findOne(userId);
+  const user = await userRepository.findOne(userId, { relations: ['voiceSettings', 'subscriptionPurchases'] });
 
   if (!user) return res.status(400).json({ message: MESSAGE_ME_NOT_FOUND });
 
