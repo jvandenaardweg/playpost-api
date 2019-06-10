407108d58f6591e9f05529ca0b0d826cd947ba2b
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-06-10 20:43:28 +0200

    Also hide colors on staging env

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,15 +1,16 @@
 import pino from 'pino';
 
 const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+const IS_STAGING = process.env.NODE_ENV === 'staging';
 
 export const logger = pino({
   base: null,
-  ...IS_PRODUCTION ? { timestamp: false } : {},
+  ...(IS_PRODUCTION || IS_STAGING) ? { timestamp: false } : {},
   prettyPrint: {
-    colorize: (IS_PRODUCTION) ? false : true,
+    colorize: (IS_PRODUCTION || IS_STAGING) ? false : true,
     levelFirst: true,
     errorLikeObjectKeys: ['err', 'error'],
-    translateTime: (IS_PRODUCTION) ? false : true,
-    ignore: (IS_PRODUCTION) ? 'time' : undefined
+    translateTime: (IS_PRODUCTION || IS_STAGING) ? false : true,
+    ignore: (IS_PRODUCTION || IS_STAGING) ? 'time' : undefined
   },
 });
