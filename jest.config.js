df7595b9c2b1df338163c1f5b553b7aa9257920b
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-07-08 14:10:40 +0200

    Do not collect coverage on npm test

--- jest.config.js
+++ jest.config.js
@@ -2,34 +2,11 @@ module.exports = {
   globals: {
     NODE_ENV: 'test'
   },
-  roots: [
-    '<rootDir>/src'
-  ],
+  roots: ['<rootDir>/src'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: [
-    'ts',
-    'tsx',
-    'js',
-    'jsx',
-    'json',
-    'node'
-  ],
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '**/*.{ts,tsx,js,jsx,json,node}',
-    '!**/node_modules/**',
-    '!**/typings/**',
-    '!**/integration/**'
-  ],
-  // coverageThreshold: {
-  //   global: {
-  //     branches: 80,
-  //     functions: 80,
-  //     lines: 80,
-  //     statements: -10
-  //   }
-  // }
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: ['**/*.{ts,tsx,js,jsx,json,node}', '!**/node_modules/**', '!**/typings/**', '!**/integration/**']
 };
