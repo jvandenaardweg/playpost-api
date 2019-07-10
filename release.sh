3c9a8626c58708d99c7344386fc2059ecfbbf7ec
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-07-10 08:55:20 +0200

    Run database:sync and caches:empty again on release

--- release.sh
+++ release.sh
@@ -13,7 +13,7 @@ echo $PACKAGE_VERSION
 npx sentry-cli releases --org "playpost" --project "playpost-api" files "$PACKAGE_VERSION" upload-sourcemaps dist/ --rewrite --validate
 
 # Makes sure subscription, language and voice data is the same between all environments
-# npm run database:sync
+npm run database:sync
 
 # Clear the Redis cache upon each new release
-# npm run caches:empty
+npm run caches:empty
