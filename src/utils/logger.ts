833aef471835faea29dc6daed431465005503868
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-08-21 15:43:59 +0200

    Do not run logger in jest

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,10 +2,11 @@ import pino from 'pino';
 
 const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 const IS_STAGING = process.env.NODE_ENV === 'staging';
+const IS_JEST = process.env.JEST_WORKER_ID;
 const IS_TEST = process.env.NODE_ENV === 'test';
 
 export const logger = pino({
-  enabled: !IS_TEST,
+  enabled: !IS_JEST,
   base: null,
   ...(IS_PRODUCTION || IS_STAGING || IS_TEST ? { timestamp: false } : {}),
   prettyPrint: {
