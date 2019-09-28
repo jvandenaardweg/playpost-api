3221fdf640bfa4b26611f369f231d87baedc45a7
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-09-28 16:55:13 +0200

    Set SSML split below limit

--- src/utils/ssml.ts
+++ src/utils/ssml.ts
@@ -1,8 +1,8 @@
 import pollySsmlSplit from 'polly-ssml-split';
 import { logger } from '../utils';
 
-export const GOOGLE_CHARACTER_HARD_LIMIT = 5000;
-export const GOOGLE_CHARACTER_SOFT_LIMIT = 4000;
+export const GOOGLE_CHARACTER_HARD_LIMIT = 4800; // just below google's api limit of 5000
+export const GOOGLE_CHARACTER_SOFT_LIMIT = 3800;
 
 export const AWS_CHARACTER_HARD_LIMIT = 3000;
 export const AWS_CHARACTER_SOFT_LIMIT = 1500;
@@ -10,7 +10,7 @@ export const AWS_CHARACTER_SOFT_LIMIT = 1500;
 export const MICROSOFT_CHARACTER_HARD_LIMIT = 1000; // 1024, but we take it safe
 export const MICROSOFT_CHARACTER_SOFT_LIMIT = 500;
 
-export const DEFAULT_SSML_SPLIT_HARD_LIMIT = 5000;
+export const DEFAULT_SSML_SPLIT_HARD_LIMIT = 4800;
 export const DEFAULT_SSML_SPLIT_SOFT_LIMIT = 3000;
 
 interface ISsmlSplitOptions {
