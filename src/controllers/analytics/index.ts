593f608dff623a35d86b6ccd1bb0948cd0e6e8b8
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-12-01 21:00:04 +0100

    Fix ts errors

--- src/controllers/analytics/index.ts
+++ src/controllers/analytics/index.ts
@@ -1,17 +1,14 @@
 
 import { Request, Response } from 'express';
+import { logger } from '../../utils';
 import { AnalyticsEventRequestBody } from './types';
 
 export class AnalyticsController {
-  constructor() { }
-
   createEvent = async (req: Request, res: Response) => {
     const eventData = req.body as AnalyticsEventRequestBody
 
-    console.log('Store in BigQuery:', eventData);
-
+    logger.info('Store in BigQuery:', eventData);
   }
-
 
   getEvents = async (req: Request, res: Response) => {
     // params:
@@ -27,8 +24,7 @@ export class AnalyticsController {
     // For example:
     // - get all plays from all articles from a publisher within 2019-01-01 till 2019-11-29
     // - get all plays from a specific article from a publisher within 2019-01-01 till 2019-11-29
-
-    console.log('Get from BigQuery:', req.query);
+    logger.info('Get from BigQuery:', req.query);
 
   }
 }
