7dfb955fb484727deeb666bc1766672fe900df5e
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-12-19 08:18:50 +0100

    Remove another unused constraint

--- src/database/migrations/1576251871318-PublicationUsers.ts
+++ src/database/migrations/1576251871318-PublicationUsers.ts
@@ -8,7 +8,6 @@ export class PublicationUsers1576251871318 implements MigrationInterface {
         await queryRunner.query(`CREATE TABLE "publication_users_user" ("publicationId" uuid NOT NULL, "userId" uuid NOT NULL, CONSTRAINT "PK_ca2169e7723d4d81bb1a51cd656" PRIMARY KEY ("publicationId", "userId"))`, undefined);
         await queryRunner.query(`CREATE INDEX "IDX_cde89c7c247524697d52aa546b" ON "publication_users_user" ("publicationId") `, undefined);
         await queryRunner.query(`CREATE INDEX "IDX_2fb335a82a7e31e6ad2ea25e84" ON "publication_users_user" ("userId") `, undefined);
-        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "UQ_7f01b2d6c8a7ff856455eaf16f9"`, undefined);
         await queryRunner.query(`ALTER TABLE "publication_users_user" ADD CONSTRAINT "FK_cde89c7c247524697d52aa546b4" FOREIGN KEY ("publicationId") REFERENCES "publication"("id") ON DELETE RESTRICT ON UPDATE NO ACTION`, undefined);
         await queryRunner.query(`ALTER TABLE "publication_users_user" ADD CONSTRAINT "FK_2fb335a82a7e31e6ad2ea25e84e" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE NO ACTION`, undefined);
     }
