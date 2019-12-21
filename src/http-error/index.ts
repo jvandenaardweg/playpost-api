7acffdce7039ec0c8bdd438ff3f819929d41fbc8
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-12-21 14:01:55 +0100

    Fix tslint error

--- src/http-error/index.ts
+++ src/http-error/index.ts
@@ -16,34 +16,11 @@ export class HttpError extends Error {
   }
 }
 
-//
-// Copyright (C) Fabian Lauer, 2018
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NO
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-// POSSIBILITY OF SUCH DAMAGE.
-//
-
 export const enum HttpStatus {
 	Continue = 100,
 	SwitchingProtocols = 101,
 	Processing = 102,
 	EarlyHints = 103,
-
-	/**
-	 * All `1xx` status codes.
-	 */
-	InformationalResponses = Continue | SwitchingProtocols | Processing | EarlyHints,
-
 
 	OK = 200,
 	Created = 201,
@@ -56,15 +33,6 @@ export const enum HttpStatus {
 	AlreadyReported = 208,
 	IMUsed = 226,
 
-	/**
-	 * All `2xx` status codes.
-	 */
-	Success = (
-		OK | Created | Accepted | NonAuthoritativeInformation | NoContent | ResetContent | PartialContent | MultiStatus |
-		AlreadyReported | IMUsed
-	),
-
-
 	MultipleChoices = 300,
 	MovedPermanently = 301,
 	Found = 302,
@@ -74,15 +42,6 @@ export const enum HttpStatus {
 	SwitchProxy = 306,
 	TemporaryRedirect = 307,
 	PermanentRedirect = 308,
-
-	/**
-	 * All `3xx` status codes.
-	 */
-	Redirection = (
-		MultipleChoices | MovedPermanently | Found | SeeOther | NotModified | UseProxy | SwitchProxy | TemporaryRedirect |
-		PermanentRedirect
-	),
-
 
 	BadRequest = 400,
 	Unauthorized = 401,
@@ -113,18 +72,6 @@ export const enum HttpStatus {
 	RequestHeaderFieldsTooLarge = 431,
 	UnavailableForLegalReasons = 451,
 
-	/**
-	 * All `4xx` error codes.
-	 */
-	ClientErrors = (
-		BadRequest | Unauthorized | PaymentRequired | Forbidden | NotFound | MethodNotAllowed | NotAcceptable |
-		ProxyAuthenticationRequired | RequestTimeout | Conflict | Gone | LengthRequired | PreconditionFailed |
-		PayloadTooLarge | URITooLong | UnsupportedMediaType | RangeNotSatisfiable | ExpectationFailed | ImATeapot |
-		MisdirectedRequest | UnprocessableEntity | Locked | FailedDependency | UpgradeRequired | PreconditionRequired |
-		TooManyRequests | RequestHeaderFieldsTooLarge | UnavailableForLegalReasons
-	),
-
-
 	InternalServerError = 500,
 	NotImplemented = 501,
 	BadGateway = 502,
@@ -135,13 +82,5 @@ export const enum HttpStatus {
 	InsufficientStorage = 507,
 	LoopDetected = 508,
 	NotExtended = 510,
-	NetworkAuthenticationRequired = 511,
-
-	/**
-	 * All `5xx` error codes.
-	 */
-	ServerErrors = (
-		InternalServerError | NotImplemented | BadGateway | ServiceUnavailable | GatewayTimeout | HTTPVersionNotSupported |
-		VariantAlsoNegotiates | InsufficientStorage | LoopDetected | NotExtended | NetworkAuthenticationRequired
-	)
+	NetworkAuthenticationRequired = 511
 }
