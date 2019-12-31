ad1ac8dd67ba001f483de5fcb37ce83c0ac17dd4
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-12-31 07:17:56 +0100

    Revert "Update to node 12.14.0"
    This reverts commit 1e49b5d245e8d76cd662b3fdbd1289aaec5dbc12.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:12.14.0
+FROM node:10.15.0
 
 # Create app directory
 WORKDIR /usr/src/app
---
+++ cloud-functions/synthesizder/index.js
@@ -0,0 +1,8 @@
+exports.synthesize = async (req, res) => {
+  const notification = req.body;
+
+
+
+
+  return res.status(200).json(req.body);
+};
