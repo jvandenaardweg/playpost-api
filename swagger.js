7727807bbe4f58dad25457dcd5bf42e7855293de
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2020-02-20 11:26:50 +0100

    Change script

---
+++ swagger.js
@@ -0,0 +1,79 @@
+const { version } = require('./package.json');
+
+module.exports = {
+  info: {
+    title: 'Playpost API',
+    description: 'This is the documentation of the Playpost API',
+    version,
+    termsOfService: 'https://playpost.app/terms',
+    contact: {
+      name: 'API Support',
+      url: 'https://playpost.app/support',
+      email: 'support@playpost.app'
+    },
+  },
+  openapi: '3.0.0',
+  servers: [
+    {
+      url: 'http://localhost:3000/{basePath}',
+      description: 'Development server',
+      variables: {
+        basePath: {
+          default: 'v1',
+        },
+      }
+    },
+    {
+      url: 'https://api.playpost.app/{basePath}',
+      description: 'Production server',
+      variables: {
+        basePath: {
+          default: 'v1',
+        },
+      }
+    },
+    {
+      url: 'https://playpost-api-test.herokuapp.com/{basePath}',
+      description: 'Test server',
+      variables: {
+        basePath: {
+          default: 'v1',
+        },
+      }
+    }
+  ],
+  components: {},
+  tags: [
+    {
+      name: 'auth',
+      description: '(public) Endpoint for logging in and requesting new passwords.'
+    },
+    {
+      name: 'users',
+      description: '(public) Endpoint for the currently logged in user.'
+    },
+    {
+      name: 'billing',
+      description: 'Billing related endpoints. Mostly Stripe.'
+    },
+    {
+      name: 'countries',
+      description: 'Countries related endpoints.'
+    },
+    {
+      name: 'languages',
+      description: 'Languages related endpoints.'
+    },
+    {
+      name: 'user',
+      description: 'Endpoint for the currently logged in user.'
+    },
+  ],
+  apis: [
+    './src/controllers/**/*.ts', 
+    './src/controllers/**/*.yaml',
+    './src/swagger-schemas/internal/**/*.yaml',
+    './src/database/entities/**/*.yaml',
+    // './src/swagger-schemas/external/stripe-only-schemas.yaml'
+  ],
+};
